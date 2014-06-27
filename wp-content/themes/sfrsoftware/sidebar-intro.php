108c55939c058554347eacac316d3d98292b0496
viethung0o0 <viethung090@gmail.com>
2014-06-27 18:24:46 +0700

    repair

--- wp-content/themes/sfrsoftware/sidebar-intro.php
+++ wp-content/themes/sfrsoftware/sidebar-intro.php
@@ -9,7 +9,7 @@
 				</ol>
 				<div class="carousel-inner">
                                     <?php
-                                        $count = 1; //sologan first asdfasfsa
+                                        $count = 1; //sologan first
                                         $featured_args = array(
                                             'posts_per_page' => 3,
                                             'post_type' => 'sologan',
@@ -20,7 +20,7 @@
                                         ?> 
                                             <div class="item active" >
                                                    <div id="saying" class="clearfix">
-                                                           <div class=" col-sm-11 col-sm-offset-1">
+                                                           <div class=" col-sm-10 col-sm-offset-1">
                                                                     <h2>
                                                                         <?php echo '<i>" </i>';
                                                                              remove_filter ('the_content', 'wpautop'); 
